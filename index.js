a4f3275ce87856656555c75f158d38111ae0172e
Mikita Lisavets <mikita.lisavets@gmail.com>
2017-07-25 20:45:02 +0300

    Update files

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = 8080;
+var port = process.env.PORT || 8080;
 var router = express.Router();
 
 router.get('/weather', function(req, res) {
